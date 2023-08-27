5675f41e3381ef753aa6e4a4b80b53febf229178
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-08-27 05:16:19 +0200

    Authentication measures & login session update

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,30 +2,40 @@ import React from "react";
 import { useState } from "react";
 
 export const LoginView = ({ onLoggedIn }) => {
-    const [Username, setUsername] = useState("");
-    const [Password, setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleSubmit = (event) => {
         // this prevents the default behavior of the form which is to reload the entire page
     event.preventDefault();
 
     const data = {
-        username: Username,
-        password: Password
+        Username: username,
+        Password: password
     };
 
     fetch("https://movies-flix-2-2c5b748a56db.herokuapp.com/login", {
-        method: "POST",
-        body: JSON.stringify(data)
-        }).then((response) => {
-            if (response.ok) {
-                onLoggedIn(Username);
-            } else {
-                alert("login failed");
-            }
-        });
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
     };
-
 
     return (
         <form onSubmit={handleSubmit}>
@@ -33,16 +43,18 @@ export const LoginView = ({ onLoggedIn }) => {
                 Username:
                 <input 
                     type="text"
-                    value={Username}
-                    onChange={(e) => setUsername(e.target.value)} 
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required 
                 />
             </label>
             <label>
                 Password:
                 <input 
                     type="password"
-                    value={Password}
-                    onChange={(e) => setPassword(e.target.value)} 
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required 
                 />
             </label>
             <button type="submit">
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,44 +5,70 @@ import { LoginView } from "../login-view/login-view";
 
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([]);
-    const [selectedMovie, setSelectedMovie] = useState(null);
-    const [user, setUser] = useState(null);
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
+  const [user, setUser] = useState(storedUser? storedUser : null);
+  const [token, setToken] = useState(storedToken? storedToken : null);
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
     useEffect(() => {
-      fetch("https://movies-flix-2-2c5b748a56db.herokuapp.com/movies")
+      if (!token) return;    
+
+      fetch("https://movies-flix-2-2c5b748a56db.herokuapp.com/movies", {
+        headers: { Authorization: `Bearer ${token}` }
+    })
         .then((response) => response.json())
         .then((data) => {
-          const moviesFromApi = data.map((movie) => {
-            return {
-              image: movie.ImagePath,
-              title: movie.Title,
-              description: movie.Description,
-              genre: movie.Genre.Name,
-              director: movie.Director.Name,             
-            };
-          });
+          
+          setMovies(data);
 
-          setMovies(moviesFromApi);
+          // const moviesFromApi = data.map((movie) => {
+          //   return {
+          //     image: movie.ImagePath,
+          //     title: movie.Title,
+          //     description: movie.Description,
+          //     genre: movie.Genre.Name,
+          //     director: movie.Director.Name,             
+          //   };
+          // });
+          // setMovies(moviesFromApi);
+
         });
-    }, []);
+    }, [token]);
     
     if (!user) {
-      return <LoginView onLoggedIn={(user) => setUser(user)} />
+      return (
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+          }}
+        />
+      );
     }
 
     if (selectedMovie) {
         return (
+            <>            
+            <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
             <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+            </>         
           );
       }
 
     if (movies.length === 0) {
-        return <div>The list is empty!</div>;
+      return (
+        <>        
+        <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+        <div>The list is empty!</div>;
+        </>
+      );
       }
     
       return (
         <div>
+          <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
           {movies.map((movie) => (
             <MovieCard
                 key={movie.id}
