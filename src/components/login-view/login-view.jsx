b1ef8e30da1da71c0f89fbe09cf43dbca7371a81
Gerald Arthur Moore <gerald.moore42@gmail.com>
2023-09-02 03:39:29 +0200

    Integ form and card elements to login/moviecards

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { useState } from "react";
+// import React from "react";
+import { React, useState } from "react";
+import { Button } from "react-bootstrap/Button"; 
+import { Form } from "react-bootstrap/Form"; 
 
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState("");
@@ -38,28 +40,31 @@ export const LoginView = ({ onLoggedIn }) => {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input 
+        <Form onSubmit={handleSubmit}>  
+                
+            <Form.Group>
+              <Form.Label>Username:</Form.Label>
+                <Form.Control 
                     type="text"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
                     required 
                 />
-            </label>
-            <label>
-                Password:
-                <input 
+            </Form.Group>
+
+            <Form.Group>
+            <Form.Label>Password:</Form.Label>
+                <Form.Control 
                     type="password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     required 
                 />
-            </label>
-            <button type="submit">
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
                 Submit
-            </button>
-        </form>
+            </Button>
+        </Form>
     );
 };
